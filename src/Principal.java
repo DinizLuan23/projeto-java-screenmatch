e94ed7bb8507e55b9aeacbf00e347df361322161
Luan Diniz <luan.diniz23@hotmail.com>
2023-10-25 14:01:41 -0300

    Classe Serie e Titulo e relacionamento de Herança

--- src/Principal.java
+++ src/Principal.java
@@ -1,12 +1,13 @@
 import diniz.luan.screenmatch.modelos.Filme;
+import diniz.luan.screenmatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
 
-        meuFilme.nome = "Avatar 2";
-        meuFilme.anoDeLancamento = 2023;
-        meuFilme.duracaoEmMinutos = 210;
+        meuFilme.setNome("Avatar 2");
+        meuFilme.setAnoDeLancamento(2023);
+        meuFilme.setDuracaoEmMinutos(210);
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
@@ -15,5 +16,12 @@ public class Principal {
 
         System.out.println(meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.obterMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.setTemporadas(10);
+        lost.setEpisodioPorTemporada(10);
+        lost.exibeFichaTecnica();
     }
 }
--- src/diniz/luan/screenmatch/modelos/Filme.java
+++ src/diniz/luan/screenmatch/modelos/Filme.java
@@ -1,28 +1,13 @@
 package diniz.luan.screenmatch.modelos;
 
-public class Filme {
-    public String nome;
-    public int anoDeLancamento;
-    public boolean incluidoNoPlano;
-    private double somaDasAvaliacoes;
-    private int totalDeAvaliacoes;
-    public int duracaoEmMinutos;
+public class Filme extends Titulo {
+    private String diretor;
 
-    public int getTotalDeAvaliacoes(){
-        return totalDeAvaliacoes;
+    public String getDiretor() {
+        return diretor;
     }
 
-    public void exibeFichaTecnica(){
-        System.out.println("Nome do diniz.luan.screenmatch.modelos.Filme: " + nome);
-        System.out.println("Ano de lançamento: " + anoDeLancamento);
-    }
-
-    public void avalia(double nota){
-        somaDasAvaliacoes += nota;
-        totalDeAvaliacoes ++;
-    }
-
-    public double obterMedia(){
-        return somaDasAvaliacoes / totalDeAvaliacoes;
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
     }
 }
